b5b618d7675fede1e001a51a51bfdb1c0f148aca
gloria larbi <Miss-Gloria>
2025-03-21 15:20:25 +0000

    worked on advert models

--- models/advert.js
+++ models/advert.js
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose"
+import { Schema, model, Types } from "mongoose"
 
 const advertSchema = new Schema({
     title: {type: String, required: true, unique: true},
