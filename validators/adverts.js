8418b4e3877fa6fc60ff8cf45c35cd46a0c8be15
gloria larbi <Miss-Gloria>
2025-03-24 15:26:15 +0000

    worked on the advert validator

--- validators/adverts.js
+++ validators/adverts.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-const addAdvertValidator = Joi.object({
+export const addAdvertValidator = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
     category: Joi.string().valid('laptops', 'smartphones', 'headphones', 'smartwatch', 'accessories').required(),
@@ -8,7 +8,7 @@ const addAdvertValidator = Joi.object({
     image: Joi.string().required(),
 });
   
-const updateAdvertValidator = Joi.object({
+export const updateAdvertValidator = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
     category: Joi.string().valid('laptops', 'smartphones', 'headphones', 'smartwatch', 'accessories').required(),
